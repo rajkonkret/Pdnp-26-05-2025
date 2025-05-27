4dd34197ae1dd278367bdf9e2e4b86a97eabafcb
rajkonkret <rajkonkret660@gmail.com>
2025-05-27 12:57:35 +0200

    [2 12a] update warunki.py

--- day_2/warunki.py
+++ day_2/warunki.py
@@ -48,3 +48,22 @@ if n > 3:
 if (n := len(a)) > 3:
     print(f"Długość wynosi {n}, więcej niż 3")
 print(n)  # 5
+
+# kolejność warunków ma znaczenie
+podatek = 0
+zarobki = int(input("Podaj zarobki (roczne) PLN"))
+if zarobki < 10_000:  # 0 - 9999 -> 0
+    podatek = 0
+elif zarobki < 40_000:  # 10_000 - 39_999 -> 0.2
+    podatek = 0.2
+elif zarobki < 100_000:
+    podatek = 0.4
+else:
+    podatek = 0.9
+
+print(f"Podatek wynosi {podatek * zarobki} pln.")
+# 5
+# Podaj zarobki (roczne) PLN1000000
+# Podatek wynosi 900000.0 pln.
+# Podaj zarobki (roczne) PLN35000
+# Podatek wynosi 0 pln.
