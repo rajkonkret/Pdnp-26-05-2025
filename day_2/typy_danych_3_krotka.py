6824bf9262cbbb4721639ffc780836bb8e22b9e0
rajkonkret <rajkonkret660@gmail.com>
2025-05-27 10:24:55 +0200

    [2 4] update typy_danych_3_krotka.py

--- day_2/typy_danych_3_krotka.py
+++ day_2/typy_danych_3_krotka.py
@@ -1,5 +1,6 @@
 # krotka, tupla - niemutowalna lista
 # pozwala efektywniej zarządzac pamięcią
+# stała - zmienna niezmienna
 
 tupla_imiona = ("Radek", "Tomek", "Zenek", "Mateusz")
 print(tupla_imiona)  # ('Radek', 'Tomek', 'Zenek', 'Mateusz')
@@ -29,3 +30,29 @@ del tupla_liczby  # usunięcie tuplii z pamięci
 print(tupla_imiona.index("Radek"))  # index 0
 print(tupla_imiona.count("Radek"))  # wystepuje 1 raz
 
+# rozpakowanie tupli
+tup = 1, 2
+print(type(tup))
+
+a, b = 1, 2
+print(a, b)  # 1 2
+
+tup2 = 1, 2, 3
+# a, b = tup2 # ValueError: too many values to unpack (expected 2)
+a, *b = tup2  # * worek na pozostałe dane
+print(a, b)  # 1 [2, 3]
+
+*a, b = tup2
+print(a, b)  # [1, 2] 3
+
+# zwróci listę posortowaną
+print(sorted(tupla_imiona))
+# ['Mateusz', 'Radek', 'Tomek', 'Zenek']
+print(tupla_imiona)  # nie zmieni oryginału
+# ('Radek', 'Tomek', 'Zenek', 'Mateusz')
+
+lista_z_tupli = list(tupla_imiona)
+print(lista_z_tupli)  # ['Radek', 'Tomek', 'Zenek', 'Mateusz']
+print(type(lista_z_tupli))  # <class 'list'>
+tupla_nowa = tuple(lista_z_tupli)
+print(type(tupla_nowa))  # <class 'tuple'>
