bfca21062b7bc556b8120ada6f2300e597053765
rajkonkret <rajkonkret660@gmail.com>
2025-05-27 09:37:03 +0200

    [2 2] update typy_danych_2_lista.py

--- day_1/typy_danych_2_lista.py
+++ day_1/typy_danych_2_lista.py
@@ -88,3 +88,50 @@ print(lista.pop(5))  # Emilia
 print(lista)  # ['Roman', 'Tomek', 'Zenek', 'Radek', 'Kuba', 'Anna']
 print(lista.pop(-2))  # Kuba
 print(lista.pop())  # Anna, usunie ostatni
+
+a = 1
+b = 3
+a = b
+print(f"{a=}, {b=}")  # a=3, b=3
+b = 9
+print(f"{a=}, {b=}")  # a=3, b=9
+
+lista_2 = lista  # a = b, kopia refeerencji, adres pamięci
+lista_copy = lista.copy()  # kopia eleemntów listy
+print(lista_2)  # ['Roman', 'Tomek', 'Zenek', 'Radek']
+print(lista)  # ['Roman', 'Tomek', 'Zenek', 'Radek']
+lista.clear()  # czyszczenie elementów z listy, krok b = 9
+print(lista)  # []
+print(lista_2)  # []
+print(lista_copy)  # ['Roman', 'Tomek', 'Zenek', 'Radek']
+print(id(lista))  # 1149112591104
+print(id(lista_2))  # 1149112591104
+print(id(lista_copy))  # 1149114400640
+
+liczby = [54, 999, 34, 22, 12.34, 567]
+print(liczby)  # [54, 999, 34, 22, 12.34, 567]
+print(type(liczby))  # <class 'list'>
+
+liczby.sort()  # <class 'list'>
+print(liczby)  # [12.34, 22, 34, 54, 567, 999], zmiana oryginalnego
+
+liczby = [54, 999, 34, 22, 12.34, 567, "A"]
+print(liczby)  # [54, 999, 34, 22, 12.34, 567, 'A']
+print(type(liczby))  # <class 'list'>
+
+# liczby.sort() # TypeError: '<' not supported between instances of 'str' and 'int'
+
+lista_copy.reverse()
+print(lista_copy)  # ['Radek', 'Zenek', 'Tomek', 'Roman']
+
+# rozpakowanie sekwencji
+tekst = "Pyth on."
+lista1 = list(tekst)
+print(lista1)  # ['P', 'y', 't', 'h', ' ', 'o', 'n', '.']
+
+lista2 = [tekst]
+print(lista2)  # ['Pyth on.']
+
+krotka = tuple(lista_copy)
+print(type(krotka))  # <class 'tuple'>
+print(krotka)  # ('Radek', 'Zenek', 'Tomek', 'Roman')
