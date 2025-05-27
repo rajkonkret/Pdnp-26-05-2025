9d6b0137aeb5d776866e9493a5449c3d4111e3c9
rajkonkret <rajkonkret660@gmail.com>
2025-05-27 14:44:57 +0200

    [2 20] update random_zad1.py

--- day_2/random_zad1.py
+++ day_2/random_zad1.py
@@ -10,3 +10,16 @@ print(random.randrange(5))  # int od 0 do 4
 
 print(random.random())  # 0.8239814245049077 float od 0 do 0.999999
 print(random.random() * 8)  # 6.576811415721201 float od 0 do 7.999999
+
+lista = [67, 45, 32, 68, 69, 90, 42]
+print(random.choice(lista))  # 67
+
+lista_kule = list(range(1, 50))  # od 1 do 49
+# print(lista_kule)
+wyn = random.choice(lista)
+lista_kule.remove(wyn)
+print(wyn)  # 32
+
+print(random.choices(lista_kule, k=6))  # losuje z powtórzeniamy, [3, 45, 18, 18, 41, 22]
+print(random.sample(lista_kule, k=6))  # losuje bez powtórzeń, [36, 41, 14, 30, 44, 20]
+print(random.sample(lista_kule, 6))  # losuje bez powtórzeń, [23, 22, 31, 43, 29, 37]
